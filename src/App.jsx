0164de4d268d3354ebf6a86deefa97fd900bbcc3
nirqqen97 <nirqqen97@gmail.com>
2023-11-14 14:14:51 +0100

    Experience + Project + data
    Done with experience section with styling
    Done with Project section with styling and move card component to another file
    Create raw data file

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import styles from "./App.module.css";
-import About from "./components/About/About";
+import { About } from "./components/About/About";
+import { Experience } from "./components/Experience/Experience";
 import { Hero } from "./components/Hero/Hero";
 import { Navbar } from "./components/Navbar/Navbar";
+import { Projects } from "./components/Projects/Projects";
 
-function App() {
+export const App = () => {
   return (
     <div className={styles.App}>
       <Navbar />
       <Hero />
       <About />
+      <Experience />
+      <Projects />
     </div>
   );
-}
-
-export default App;
+};
