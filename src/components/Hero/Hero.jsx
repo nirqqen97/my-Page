06302acb3df5b0c1c21a8057effc44c93a483e22
nirqqen97 <nirqqen97@gmail.com>
2023-11-15 13:45:53 +0100

    styling

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,17 +6,21 @@ export const Hero = () => {
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Dimitry</h1>
-        <p className={styles.description}> I'm a Frontend developer with 2 years of experience using React and Node.js. Reach out if you'd like to learn more </p>
+        <p className={styles.description}>
+          {" "}
+          I'm a Frontend developer with 2 years of experience using React and
+          Node.js. Reach out if you'd like to learn more{" "}
+        </p>
         <a href="mailto:ivanov1dmytro@gmail.com" className={styles.contactBtn}>
           Contact Me
         </a>
       </div>
-      <div className={styles.circle}> 
-      <img
-        src={getImageUrl("hero/me.png")}
-        alt="Hero Image of me"
-        className={styles.heroImg}
-      />
+      <div className={styles.circle}>
+        <img
+          src={getImageUrl("hero/me.png")}
+          alt="Hero Image of me"
+          className={styles.heroImg}
+        />
       </div>
       <div className={styles.topBlur}></div>{" "}
       <div className={styles.bottomBlur}></div>
